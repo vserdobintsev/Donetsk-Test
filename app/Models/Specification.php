d1f27094388bdbb8e86437a43bf969991874b114
Владислав Сердобинцев <serdobincev97@gmail.com>
2021-10-26 15:33:46 +0300

    Added specification model

---
+++ app/Models/Specification.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Specification extends Model
+{
+    use HasFactory;
+    protected $fillable = ['product_id', 'spec_type_id', 'value'];
+}
